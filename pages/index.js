1e40af5face549e9d690efebbda3241cd06b9df8
Claudio Antunes <iamclaudioantunes@gmail.com>
2024-11-25 18:42:39 -0300

    Adicionando .editorconfig

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h2>Oi, PrisÇolba! Se eu não morrer, agora vai! Obrigado pelo apoio de sempre! ❤️</h2>
+    return <h2>Yasmin M., pede com jeitinho que eu te faço um carinho! Amizade é pra isso ❤️</h2>
 }
 
 export default Home;
